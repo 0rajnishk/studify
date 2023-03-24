d7491c869edd340618d511f1dfc4e64253c9199f
Rajnish kumar <0rajnishk@gmail.com>
2023-03-24 19:07:14 +0530

    utils.py list of blocked email and admin email -->

--- utils.py
+++ utils.py
@@ -1,14 +1,35 @@
 from functools import wraps
 from flask import session, redirect, url_for, request
 # decorator for routes that should be accessible only by logged in users
+
+# Define a list of allowed domains
+allowed_domains = ['ds.study.iitm.ac.in',
+                   'study.iitm.ac.in', 'ds.study.iitm.ac.in']
+
+# Define a list of blocked emails
+blocked_emails = ['user@example.com']
+
+# Define a list of admin emails
+admin_emails = ['surajnish02@gmail.com', 'studify.iitm@gmail.com']
+
+
 def login_required(func):
     @wraps(func)
     def decorated_function(*args, **kwargs):
         if 'profile' not in session:
             return redirect(url_for('login', next=request.url))
-        email = session['profile']['email']
-        if email.endswith('@ds.study.iitm.ac.in'):
-            return func(*args, **kwargs)
+
+        user_info = session['profile']
+        email = user_info['email']
+        domain = email.split('@')[-1]
+
+        if domain in allowed_domains or email in admin_emails:
+            if email in blocked_emails:
+                return 'You are blocked. Contact admin for more details'
+            else:
+                session.permanent = True
+                return func(*args, **kwargs)
         else:
-            return f'You are not authorized to access this page. Please use an authorized email address ({email}).'
+            return 'You are not authorized to access this page. Please use student email address.'
+
     return decorated_function
