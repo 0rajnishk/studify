ded19228cc0868fb95f70d66048fbb45a5cc98f2
Rajnish kumar <0rajnishk@gmail.com>
2023-05-06 20:00:05 +0530

    ..

--- static/js/pyq_choose.js
+++ static/js/pyq_choose.js
@@ -44,31 +44,3 @@ const submitBtn = document.getElementById('submit-btn');
 submitBtn.addEventListener('click', submitClicked);
 
 
-
-// function categoryChanged() {
-//   const categorySelect = document.getElementById('category-select');
-//   const optionSelect = document.getElementById('option-select');
-  
-//   // Clear the options in the option select box
-//   optionSelect.innerHTML = '<option value="">Select an option</option>';
-  
-//   if (categorySelect.value === '') {
-//     return;
-//   }
-  
-//   // Make a request to your Flask app to get the options for the selected category
-//   fetch(`/get_options/${categorySelect.value}`)
-//     .then(response => response.json())
-//     .then(options => {
-//       // Add the options to the option select box
-//       options.forEach(option => {
-//         const optionElement = document.createElement('option');
-//         optionElement.value = option.value;
-//         optionElement.textContent = option.label;
-//         optionSelect.appendChild(optionElement);
-//       });
-//     });
-// }
-
-// const categorySelect = document.getElementById('category-select');
-// categorySelect.addEventListener('change', categoryChanged);
